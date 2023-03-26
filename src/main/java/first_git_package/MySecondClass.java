418b97d7a82e24cdc29b0be515bb0bbc7bee132f
Yusuf Bas <yusufhbas@yahoo.com>
2023-03-25 23:10:43 -0400

    update second  message

--- src/main/java/first_git_package/MySecondClass.java
+++ src/main/java/first_git_package/MySecondClass.java
@@ -3,5 +3,6 @@ package first_git_package;
 public class MySecondClass {
     public static void main(String[] args) {
         System.out.println("this is my second class");
+        System.out.println("updating existing code");
     }
 }
